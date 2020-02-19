10b225f62bd3a6643bdb76f5457a4c2251299440
Colin Nolan <cn580@alumni.york.ac.uk>
2020-02-19 19:10:26 +0000

    Adds token for super user.

--- shinobi/generate-super-config.sh
+++ shinobi/generate-super-config.sh
@@ -4,6 +4,7 @@ set -euf -o pipefail
 
 configDestination="${SHINOBI_INSTALL_DIRECTORY}/super.json"
 
-jq -n "[.mail=\"${SHINOBI_SUPER_USER_EMAIL}\" | .pass=\"${SHINOBI_SUPER_USER_PASSWORD}\"]" > "${configDestination}" 
+jq -n "[.mail=\"${SHINOBI_SUPER_USER_EMAIL}\" | .pass=\"${SHINOBI_SUPER_USER_PASSWORD}\" | .tokens=[\"${SHINOBI_SUPER_USER_TOKEN}\"]]" \
+    > "${configDestination}"
 
 >&2 echo "Generated Shinobi super configuration: ${configDestination}"
